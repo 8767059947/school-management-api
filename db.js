70d7c910bfe87f76b70c5ef4661cffc60d47f76b
Veer Pardeshi <139315910+8767059947@users.noreply.github.com>
2025-08-07 17:09:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: process.env.MYSQLHOST, // Changed from DB_HOST
-  user: process.env.MYSQLUSER, // Changed from DB_USER
-  password: process.env.MYSQLPASSWORD, // Changed from DB_PASSWORD
-  database: process.env.MYSQLDATABASE, // Changed from DB_NAME
-  port: process.env.MYSQLPORT, // Changed from PORT
+  host: process.env.DB_HOST, // Changed from DB_HOST
+  user: process.env.DB_USER, // Changed from DB_USER
+  password: process.env.DB_PASSWORD, // Changed from DB_PASSWORD
+  database: process.env.DB_NAME, // Changed from DB_NAME
+  port: process.env.PORT, // Changed from PORT
 });
